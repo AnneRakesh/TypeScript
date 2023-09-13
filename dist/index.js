74bc97f5bb3747a0c02d2386af78d291c49ecf76
Anne Rakesh <anne.rakesh2@globallogic.com>
2023-09-13 17:23:21 +0530

    Any Type learnt

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,20 @@
 "use strict";
-let test = "Hello World";
+let age = 21;
+let userName = "Anne Rages";
+let isDone = false;
+// isDone = "string"
+let isDualDone = true;
+isDone = isDualDone;
+let test;
+test = "test";
+test = 20;
+// function testFn (array:string) {
+function testFn(array) {
+    // if you do not mention the type of the args it will not accept and not let you compile the function
+    // There are two ways to get rid of this error for compile 1, give annotate & 2, turn off the no implicitly return  
+    console.log(array);
+}
+// testFn("Test");
+testFn(45455545656); // as we have provided ":any annotate " it is accepting any data type
+testFn(" Hello World"); // Here as a string
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-let test: string = "Hello World";
+let age: number = 21;
+let userName: string = "Anne Rages";
+let isDone = false;
+// isDone = "string"
+let isDualDone:boolean = true;
 
-if (test) test = "Succuss";
+isDone = isDualDone
 
-test = "Again"
-test = "Again"
-test = "Again"
+let test;
+
+test = "test";
+test = 20;
+
+
+
+
+// function testFn (array:string) {
+function testFn (array:any) {
+    // if you do not mention the type of the args it will not accept and not let you compile the function
+    // There are two ways to get rid of this error for compile 1, give annotate & 2, turn off the no implicitly return  
+    console.log(array)
+}
+
+
+
+// testFn("Test");
+testFn(45455545656); // as we have provided ":any annotate " it is accepting any data type
+testFn(" Hello World"); // Here as a string
